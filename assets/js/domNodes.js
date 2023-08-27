48d41c80f18ce8f56568e87cc9383edb71a45f70
MahshadOliaie <mahshadoliaie@gmail.com>
2023-08-28 01:52:04 +0330

    refactor/ MV*

--- assets/js/domNodes.js
+++ assets/js/domNodes.js
@@ -4,4 +4,5 @@ let addBtn = document.querySelector(".tasks__header__add__addBtn");
 let newTask = document.querySelector(".tasks__header__add__newTask");
 let tasksBox = document.querySelector(".tasks__body");
 let checkbox = [];
-let moreBtn = [];
+
+let root = document.querySelector(".tasks__body")
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,58 @@
 
 
-let howManyTasks = 0;
 let doneTasks = [];
-
+let tasks = [];
 
 
 
 
 
 function add() {
+
     if (newTask.value !== "") {
-        const value = newTask.value;
-        let div = document.createElement("div");
-        div.classList.add("tasks__body__task");
-        div.innerHTML = `<svg class="svg" fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-        viewBox="0 0 60 60" xml:space="preserve">
-   <g>
-       <path d="M30,16c4.411,0,8-3.589,8-8s-3.589-8-8-8s-8,3.589-8,8S25.589,16,30,16z"/>
-       <path d="M30,44c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,44,30,44z"/>
-       <path d="M30,22c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,22,30,22z"/>
-   </g>
-   </svg>`;
-
-        let check = document.createElement("div");
-        check.classList.add("tasks__body__task__check");
-        let content = document.createElement("p");
-        content.classList.add("tasks__body__task__content");
-        let remove = document.createElement("p");
-        remove.textContent = "remove"
-        remove.classList.add("remove");
-        let edit = document.createElement("adit");
-        edit.textContent = "edit"
-        let more = document.createElement("div");
-        more.classList.add("more");
-        more.appendChild(edit);
-        more.appendChild(remove);
-        div.appendChild(more);
-        div.appendChild(check);
-        div.appendChild(content);
-        tasksBox.appendChild(div);
-        content.textContent = value;
-        howManyTasks++;
-
-        moreBtn = document.querySelectorAll(".svg");
-        if (moreBtn.length !== 0) {
-            for (const item of moreBtn) {
-                item.addEventListener("click", moreFN);
-            }
-        }
-
+        tasks.push(newTask.value);
+        render();
         percent();
 
         newTask.value = "";
         newTask.focus();
-
-        checkbox = document.querySelectorAll(".tasks__body__task__check");
-        if (checkbox.length !== 0) {
-            for (const item of checkbox) {
-                item.addEventListener("click", done)
-            }
-        }
 
     }
 
 }
 
 
-function moreFN() {
-    this.nextSibling.classList.toggle("show");
+
+
+function done(item) {
+    document.getElementById(`check${item}`).classList.toggle("done");
+    doneTasks = [...document.querySelectorAll(".done")];
+
+    percent();
 }
+
+
+
+let includeShow = [];
+function moreFN(index) {
+
+    document.getElementById(`more${index}`).classList.toggle("show");
+
+    includeShow = document.querySelectorAll(".show");
+
+
+    if (includeShow.length > 1) {
+        for (const item of includeShow) {
+            item.classList.remove("show");
+        }
+        document.getElementById(`more${index}`).classList.add("show");
+    }
+
+
+
+
+}
+
 
 function addHandler(evt) {
     if (evt.key === 'Enter') {
@@ -82,18 +66,45 @@ function addHandler(evt) {
 
 
 
-function done() {
-    this.classList.toggle("done");
-    doneTasks = document.querySelectorAll(".done");
+
+
+
+
+
+function remove(index, item) {
+    if (doneTasks.includes(item)) {
+        doneTasks.splice(doneTasks.indexOf(item), 1);
+    }
+    tasks.splice(index, 1);
     percent();
+    debugger
+    render();
+
 }
 
 
 function percent() {
-    let percent = ((doneTasks.length) / howManyTasks) * 100;
+    let percent = ((doneTasks.length) / (tasks.length)) * 100;
 
     document.querySelector(".tools__progress__diagram__done").style.width = `${percent}%`;
     document.querySelector(".tools__progress__percent__number").textContent = Math.floor(percent) + "%"
+}
+
+
+
+function render() {
+    let template = tasks.map((item, index) => {
+        return `<div class="tasks__body__task"><div class="svg" onclick='moreFN(${index})'><svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xml: space="preserve">
+        <g>
+            <path d="M30,16c4.411,0,8-3.589,8-8s-3.589-8-8-8s-8,3.589-8,8S25.589,16,30,16z"></path>
+            <path d="M30,44c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,44,30,44z"></path>
+            <path d="M30,22c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,22,30,22z"></path>
+        </g>
+    </svg></div><div class="more" id="more${index}"><p class="edit">edit</p><p class="remove" onclick='remove(${index})'>remove</p></div><div class="tasks__body__task__check" onclick='done("${item}")' id="check${item}"></div><p class="tasks__body__task__content">${item}</p></div>`
+    }).join("");
+
+    root.innerHTML = template;
+
 }
 
 
