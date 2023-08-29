0fa0b0e4c7cc057c9d46254e33b8787d195e5e10
MahshadOliaie <mahshadoliaie@gmail.com>
2023-08-30 02:14:26 +0330

    debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,6 +125,7 @@ function searchHandler() {
 
 
 function remove(index, item) {
+    document.getElementById(`more${index}`).classList.remove("show");
     deleted.push(item);
     tasks.splice(index, 1);
     if (doneTasksList.includes(item)) {
@@ -156,15 +157,17 @@ function undo() {
 
 
 function editFn(item, index) {
+    document.getElementById(`more${index}`).classList.remove("show");
     let editedTodo = prompt("edit todo", item);
     if (editedTodo !== null) {
         tasks[index] = editedTodo;
         if (doneTasksList.includes(item)) {
             doneTasksList[doneTasksList.indexOf(item)] = editedTodo;
         }
+        render(tasks);
     }
 
-    render(tasks);
+    
 
     if (editedTodo == "") {
         remove(index)
@@ -184,22 +187,15 @@ function percent() {
 
 function render(array) {
     let template = array.map((item, index) => {
-        if (doneTasksList.includes(item)) {
+
             return `<div class="tasks__body__task"><div class="svg" onclick='moreFN("${index}")'><svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xml: space="preserve">
             <g>
                 <path d="M30,16c4.411,0,8-3.589,8-8s-3.589-8-8-8s-8,3.589-8,8S25.589,16,30,16z"></path>
                 <path d="M30,44c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,44,30,44z"></path>
                 <path d="M30,22c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,22,30,22z"></path>
             </g>
-        </svg></div><div class="more" id="more${index}"><p class="edit" onclick='editFn("${item}" , ${index})'>edit</p><p class="remove" onclick='remove(${index},"${item}")'>remove</p></div><div class="tasks__body__task__check done" onclick='done("${item}" , ${index})' id="check${item}"></div><p class="tasks__body__task__content">${item}</p></div>`
-        } else
-            return `<div class="tasks__body__task"><div class="svg" onclick='moreFN("${index}")'><svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xml: space="preserve">
-        <g>
-            <path d="M30,16c4.411,0,8-3.589,8-8s-3.589-8-8-8s-8,3.589-8,8S25.589,16,30,16z"></path>
-            <path d="M30,44c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,44,30,44z"></path>
-            <path d="M30,22c-4.411,0-8,3.589-8,8s3.589,8,8,8s8-3.589,8-8S34.411,22,30,22z"></path>
-        </g>
-    </svg></div><div class="more" id="more${index}"><p class="edit" onclick='editFn("${item}" , ${index})'>edit</p><p class="remove" onclick='remove(${index},"${item}")'>remove</p></div><div class="tasks__body__task__check" onclick='done("${item}" , ${index})' id="check${item}"></div><p class="tasks__body__task__content">${item}</p></div>`
+        </svg></div><div class="more" id="more${index}"><p class="edit" onclick='editFn("${item}" , ${index})'>edit</p><p class="remove" onclick='remove(${index},"${item}")'>remove</p></div><div class="tasks__body__task__check ${(doneTasksList.includes(item))? "done" : ""}" onclick='done("${item}" , ${index})' id="check${item}"></div><p class="tasks__body__task__content">${item}</p></div>`
+    
     }).join("");
 
     root.innerHTML = template;
