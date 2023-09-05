a36fa1a450d6eeaf18afec3fe99f4881cb42696c
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-06 01:56:36 +0330

    fix theme and background picker

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,27 +1,41 @@
 
 let colorPickers = document.querySelectorAll(".tools__themes__colors__color");
 let bgPicker = document.querySelectorAll(".tools__backgrounds__colors__color");
+let currentTheme = "theme-1"
+let currentBg = "bg-1"
+function changeTheme() {
 
-
-function changeTheme(){
+    document.getElementById(`${currentTheme}`).classList.remove("current")
+    document.getElementById(`${currentBg}`).classList.remove("current")
+    currentBg = "bg-1";
+    document.getElementById("bg-1").classList.add("current")
     let id = this.getAttribute("id");
-    document.body.setAttribute("class" , `${id}`)
+    currentTheme = id;
+    document.body.setAttribute("class", `${id}`)
+    document.body.style.background = getComputedStyle(document.getElementById("bg-1"), null).getPropertyValue('background');
+    this.classList.add("current")
 }
 
 
 
-function changeBG(){
-    let bg = getComputedStyle(this,null).getPropertyValue('background');
+function changeBG() {
+
+    document.getElementById(`${currentBg}`).classList.remove("current")
+
+    let id = this.getAttribute("id")
+    let bg = getComputedStyle(this, null).getPropertyValue('background');
     document.body.style.background = bg;
+    currentBg = id;
+    this.classList.add("current")
 }
 
 
 
 for (const theme of colorPickers) {
-    theme.addEventListener("click" , changeTheme);
-    
+    theme.addEventListener("click", changeTheme);
+
 }
 
 for (const bg of bgPicker) {
-    bg.addEventListener("click" , changeBG);
+    bg.addEventListener("click", changeBG);
 }
