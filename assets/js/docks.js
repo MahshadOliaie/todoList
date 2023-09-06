b84d1d932510ca406fce5d967674748c7bd74805
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-06 17:48:23 +0330

    add title for notes section

--- assets/js/docks.js
+++ assets/js/docks.js
@@ -5,18 +5,21 @@ let todoBtn = document.querySelector(".dock__todolist");
 let Container = document.querySelector(".tasks__body");
 let selected;
 let notesList = document.querySelector(".notesList");
+let notesHeader = document.querySelector(".notes__header");
 
 function changeToNotes(){
     selected = document.querySelector(".selected");
     notesList.classList.remove("dnone");
+    notesHeader.classList.remove("dnone")
     selected.classList.remove("selected");
-    Container.innerHTML = `<textarea name="text" id="text" cols="30" rows="10" class="note" placeholder="write here..." autofocus></textarea>`
+    Container.innerHTML = `<textarea name="text" id="text" cols="30" rows="10" class="note" placeholder="write here..."></textarea>`
     notesBtn.classList.add("selected")
 }
 
 
 function changeToTodo(){
     notesList.classList.add("dnone");
+    notesHeader.classList.add("dnone")
     selected = document.querySelector(".selected");
     selected.classList.remove("selected");
     render(tasks);
