4b2233db9ceb509ad05a938adc5c92b3ea9e4eb6
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-07 01:26:26 +0330

    compelete notes part

--- assets/js/docks.js
+++ assets/js/docks.js
@@ -10,14 +10,24 @@ let plus = document.getElementById("plus");
 let currentNote;
 let myNotes = [];
 let noteListBody = document.querySelector(".notesList__body");
+let titlebox = document.querySelector(".notes__header input");
+let noteBody = document.getElementById("text");
+let trash = document.getElementById("trash")
 
 function changeToNotes() {
     selected = document.querySelector(".selected");
     notesList.classList.remove("dnone");
-    notesHeader.classList.remove("dnone")
+    notesHeader.classList.remove("dnone");
     selected.classList.remove("selected");
-    Container.innerHTML = `<textarea name="text" id="text" cols="30" rows="10" class="note" placeholder="write here..."></textarea>`
-    notesBtn.classList.add("selected")
+    notesBtn.classList.add("selected");
+    let id = currentNote.getAttribute("id");
+    renderNotes((id-1));
+}
+
+function renderNotes(index) {
+    titlebox.value = myNotes[index].title;
+    Container.innerHTML = `<textarea name="text" id="text" cols="30" rows="10" class="note" placeholder="write here..." >${myNotes[index].body}</textarea>`
+    noteBody = document.querySelector(".tasks__body textarea");
 }
 
 
@@ -32,24 +42,38 @@ function changeToTodo() {
 
 
 
-function openNote(id) {
+function openNote(id, index) {
     document.querySelector(".currentNote").classList.remove("currentNote");
     document.getElementById(id).classList.add("currentNote");
+    renderNotes(index);
+    currentNote = document.querySelector(".currentNote")
 
-    
+
 }
 
 
 
 function addNote() {
-    myNotes.push({"id":myNotes.length + 1, "title":"title..." , "body":"text..."})
+    myNotes.push({"id": myNotes.length + 1, "title": "", "body": "" })
+    currentNote = document.querySelector(".currentNote");
+    if (currentNote){
+        currentNote.classList.remove("currentNote");
+        currentNote = document.querySelector(".currentNote");
+    }
     notesListRender();
     document.getElementById(`${myNotes.length}`).classList.add("currentNote");
+    currentNote = document.querySelector(".currentNote");
+    renderNotes((myNotes.length - 1));
 }
 
 function notesListRender() {
-    let template = myNotes.map(item => {
-        return `<div class="notesList__body__note" onclick="openNote(${item.id})" id="${item.id}">
+    let id;
+    if (currentNote){
+        id = currentNote.getAttribute("id");
+    }
+        
+    let template = myNotes.map((item, index) => {
+        return `<div class="notesList__body__note ${(item.id == id) ? "currentNote" : ""}" onclick="openNote(${item.id},${index})" id="${item.id}">
         <h2>${item.title}</h2>
         <p>${item.body}</p>
     </div>`
@@ -60,6 +84,44 @@ function notesListRender() {
 
 
 
+function changeTitle() {
+    let value = titlebox.value;
+    let id = currentNote.getAttribute("id");
+    myNotes[id - 1].title = value;
+
+    notesListRender();
+}
+
+
+
+
+function changeBody() {
+    let value = noteBody.value;
+    let id = currentNote.getAttribute("id");
+    myNotes[id - 1].body = value;
+
+    notesListRender();
+}
+
+
+
+
+// function trashFn(){
+//     let id = currentNote.getAttribute("id");
+//     myNotes.splice((id-1) , 1);
+//     notesListRender();
+//     renderNotes((myNotes.length-1));
+//     document.getElementById(`${myNotes.length}`).classList.add("currentNote");
+// }
+
+
+
+
+
+
 notesBtn.addEventListener("click", changeToNotes);
 todoBtn.addEventListener("click", changeToTodo);
 plus.addEventListener("click", addNote)
+titlebox.addEventListener("keyup", changeTitle)
+Container.addEventListener("keyup", changeBody)
+trash.addEventListener("click" , trashFn);
